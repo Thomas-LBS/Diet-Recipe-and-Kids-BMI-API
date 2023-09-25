a7c7f9902ea5800c7d435d2e86484658e3f39b63
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-09-25 14:54:12 +0200

    index.js filemodified

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const recipesAPI = new APIHandler("http://localhost:8000");
+// const recipesAPI = new APIHandler("http://localhost:8000");
 
-window.onload=()=>{
-    recipesAPI.getOneRegister(2)
-    .then(recipe=>{
-        console.log(recipe)
-    })
-    .catch(error=>{
-        console.log("error in getting recipe",)
-    })
-    recipesAPI.getOneRegisterByTitle("Spinach Scrambled Eggs")
-    .then(recipe=>{
-        console.log(recipe)
-    })
-    .catch(error=>{
-        console.log("error in getting recipe",)
-    })
+// window.onload=()=>{
+//     recipesAPI.getOneRegister(2)
+//     .then(recipe=>{
+//         console.log(recipe)
+//     })
+//     .catch(error=>{
+//         console.log("error in getting recipe",)
+//     })
+//     recipesAPI.getOneRegisterByTitle("Spinach Scrambled Eggs")
+//     .then(recipe=>{
+//         console.log(recipe)
+//     })
+//     .catch(error=>{
+//         console.log("error in getting recipe",)
+//     })
     
-}
+// }
